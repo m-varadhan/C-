f08544d0c7bbacdc7e201143632378d786f2cc3e
Varadhan M <varadhan.work@gmail.com>
2016-03-27 16:38:27 +0530

    With class to check the scope get destructed without delete

--- boost/scoped_ptr-1.1.cc
+++ boost/scoped_ptr-1.1.cc
@@ -1,12 +1,32 @@
 #include <boost/scoped_ptr.hpp>
 #include <iostream>
 
+class MyInt {
+	int i ;
+	public:
+		MyInt(int _i) : i(_i) {
+			if(_i == 2) {
+				throw "invalid value" ;
+			}
+			std::cout << "Success Constructor MyInt: " << i << "\n";
+		}
+		~MyInt() {
+			std::cout << "Called Destructor MyInt: " << i << "\n";
+		}
+		int operator *() { return i; };
+		operator int() { return i; };
+};
+
 int main()
 {
-  boost::scoped_ptr<int> p{new int{1}};
+  boost::scoped_ptr<MyInt> p{new MyInt{1}};
   std::cout << *p << '\n';
-  p.reset(new int{2});
+  try {
+  p.reset(new MyInt{2});
+  }
+  catch(char const *str) { } 
   std::cout << *p.get() << '\n';
+  std::cout << std::boolalpha << static_cast<bool>(p) << '\n';
   p.reset();
   std::cout << std::boolalpha << static_cast<bool>(p) << '\n';
 }
